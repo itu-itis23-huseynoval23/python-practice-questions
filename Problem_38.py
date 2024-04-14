ee0749ee107d860c39ec089b2f9079430c3dfac8
itu-itis23-huseynoval23 <huseynoval23@itu.edu.tr>
2024-04-15 02:11:34 +0300

    Problem38 is uploaded

---
+++ Problem_38.py
@@ -0,0 +1,6 @@
+#  Print out a message addressed to them that tells them the year 
+# that they will turn 100 years old), 
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+year = 2024 - age + 100
+print(f"{name}, you will be 100 years old in the year {year}")
